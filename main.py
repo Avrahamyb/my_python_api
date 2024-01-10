f9042f9c5493184b5cdc6446c9c61b15d2f551e5
avrahamyb <beharavraham@gmail.com>
2024-01-10 10:27:07 +0100

    c

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+data = {
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'Example City'
+    }
+
+    
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run()
